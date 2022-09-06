fa054407c9a6213ff295087f2eadda75c652310e
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-09-06 12:43:26 +0000

    chacha

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,14 +1,29 @@
 class ListsController < ApplicationController
   def new
-    @data = List.new
+  @data = List.new
+  end
+
+  def create
+  list = List.new(list_params)
+  list.save
+  redirect_to chao_path(list.id)
   end
 
   def index
+  @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def list_params
+    params.require(:List).permit(:name,:body)
+  end
+
 end
